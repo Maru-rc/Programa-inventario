354a79b3eae712d77ff72fcaab3494e069a67473
Maru-rc <ruedamanuel007@gmail.com>
2026-03-20 10:03:03 -0500

    Add files via upload
    Agregue comentarios en el codigo

--- Funciones.py
+++ Funciones.py
@@ -1,55 +1,58 @@
-def agregar_producto (inventario):
-    nombre = input("\nIngresa el nombre del producto: ")
-    for producto in inventario:
-        if producto["nombre"] == nombre:
-            cantidad = int(input("Ingresa la cantidad que vas a llevar: "))
-            producto ["cantidad"] += cantidad
+#Todas las funciones hacen lo que su nombre dice
+
+def agregar_producto (inventario): 
+    nombre = input("\nIngresa el nombre del producto: ") 
+    for producto in inventario: #Ciclo for que recorre cada producto del inventario
+        if producto["nombre"] == nombre: #Esto es para verificar si el producto que se va a ingresar ya esta en el inventario
+            cantidad = int(input("Ingresa la cantidad que vas a llevar: ")) #Variable que guarda la cantidad de producto
+            producto ["cantidad"] += cantidad #Aca se suma la cantidad nueva con la anterior registrada
             return
         
-    intentos_bucle = 0
-    while True:
-        if intentos_bucle == 3:
+    intentos_bucle = 0 #Cantidad de veces que se ha intentado ingresar un dato y ha sucedido algun error
+    while True: 
+        if intentos_bucle == 3: #Si ya van 3 veces que el usuario ha tenido un error lo saca del bucle 
             print("Error, numero maximo de intentos alcanzado")
-            return
+            return 
         try:
-             precio = int(input("Ingresa el precio del producto: "))
-             if precio < 0:
+             precio = int(input("Ingresa el precio del producto: ")) 
+             if precio < 0: #Aca mira si el precio ingresado es negativo, si es negativo lo cuenta como error
                  print("Por favor ingresa un numero mayor que 0")
-                 intentos_bucle += 1
+                 intentos_bucle += 1 
                  continue
-             break
-        except ValueError:
+             break #Si el dato ingresado es positivo y efectivamente es un numero se sale del ciclo while
+        except ValueError: #Excepcion por si ingresa algo diferente a un numero
             print("Por favor ingresa una cantidad valida\n")
             intentos_bucle += 1
     
     intentos_bucle = 0
+    #Esto ciclo while hace lo mismo que el ciclo de arriba, lo unico diferente es el inventario.append
     while True:
         if intentos_bucle == 3:
             print("Error, numero maximo de intentos alzancado")
             return
         try:
-            cantidad = int(input("Ingresal la cantidad que vas a llevar: "))
+            cantidad = int(input("Ingresa la cantidad que vas a llevar: "))
             if cantidad < 0:
                 print("Por favor ingresa un numero mayor que 0")
                 intentos_bucle += 1
                 continue
-            inventario.append({"nombre":nombre,"precio":precio,"cantidad":cantidad})
+            inventario.append({"nombre":nombre,"precio":precio,"cantidad":cantidad}) #Si no hay ningun error se ingresa el producto en el inventario
             break
         except ValueError:
             print("Por favor ingresa una cantidad valida\n")
             intentos_bucle += 1
     
 def mostrar_inventario (inventario):
-    if len(inventario) != 0:
-        for producto in inventario:
-            print(f"Producto: {producto["nombre"]} | Precio: ${producto["precio"]} | cantidad: {producto["cantidad"]}")
-    else:
+    if len(inventario) != 0: #Esto valida que haya tansiquiera 1 producto
+        for producto in inventario: #Ciclo que recorre cada uno de los productos en el inventario
+            print(f"Producto: {producto["nombre"]} | Precio: ${producto["precio"]} | cantidad: {producto["cantidad"]}") #Y pues aja aca imprime los productos
+    else: #Si no hay ningun producto imprime lo de abajo
         print("No hay nada registrado aun")
 
 def calcular_estadisticas(inventario):
-    valor_total_inventario = 0
-    total_productos_registrados = len(inventario)
+    valor_total_inventario = 0 
+    total_productos_registrados = len(inventario) #Variable que almacena la cantidad de productos registrados
     for producto in inventario:
-        valor_total_producto = producto["precio"] * producto["cantidad"]
-        valor_total_inventario += valor_total_producto
+        valor_total_producto = producto["precio"] * producto["cantidad"] #Esto es una variable que calcula el valor total de cada producto
+        valor_total_inventario += valor_total_producto #Aca simplemente se suma el valor total del inventario con el valor total de cada producto
     print(f"Productos registrados : {total_productos_registrados} | valor total del inventario: ${valor_total_inventario}")
--- inventario.py
+++ inventario.py
@@ -1,61 +1,8 @@
-inventario = []
-salir_bucle = 0
-def agregar_producto ():
-    nombre = input("\nIngresa el nombre del producto: ")
-    for producto in inventario:
-        if producto["nombre"] == nombre:
-            cantidad = int(input("Ingresa la cantidad que vas a llevar: "))
-            producto ["cantidad"] += cantidad
-            return
-        
-    intentos_bucle = 0
-    while True:
-        if intentos_bucle == 3:
-            print("Error, numero maximo de intentos alcanzado")
-            return
-        try:
-             precio = int(input("Ingresa el precio del producto: "))
-             if precio < 0:
-                 print("Por favor ingresa un numero mayor que 0")
-                 intentos_bucle += 1
-                 continue
-             break
-        except ValueError:
-            print("Por favor ingresa una cantidad valida\n")
-            intentos_bucle += 1
-    
-    intentos_bucle = 0
-    while True:
-        if intentos_bucle == 3:
-            print("Error, numero maximo de intentos alzancado")
-            return
-        try:
-            cantidad = int(input("Ingresal la cantidad que vas a llevar: "))
-            if cantidad < 0:
-                print("Por favor ingresa un numero mayor que 0")
-                intentos_bucle += 1
-                continue
-            inventario.append({"nombre":nombre,"precio":precio,"cantidad":cantidad})
-            break
-        except ValueError:
-            print("Por favor ingresa una cantidad valida\n")
-            intentos_bucle += 1
+import Funciones #Aca importo las funciones del archivo Funciones.py
+inventario = [] #Lista que guarda los diccionarios con el nombre precio y cantidad de producto
+salir_bucle = 0 #Variable para salir del bucle While
 
-def mostrar_inventario ():
-    if len(inventario) != 0:
-        for producto in inventario:
-            print(f"Producto: {producto["nombre"]} | Precio: ${producto["precio"]} | cantidad: {producto["cantidad"]}")
-    else:
-        print("No hay nada registrado aun")
-
-def calcular_estadisticas():
-    valor_total_inventario = 0
-    total_productos_registrados = len(inventario)
-    for producto in inventario:
-        valor_total_producto = producto["precio"] * producto["cantidad"]
-        valor_total_inventario += valor_total_producto
-    print(f"Productos registrados : {total_productos_registrados} | valor total del inventario: ${valor_total_inventario}")
-
+#Este es el buclee While que ejecuta el codigo principal, osea el menu
 while salir_bucle == 0:
     print("""
 Hola, que quieres hacer?
@@ -65,21 +12,24 @@ Hola, que quieres hacer?
 3. Calcular Estadisticas
 4. Salir
           """)
-    opcion = input("Escoge una opcion: ")
-
+    opcion = input("Escoge una opcion: ") #Variable que guarda la opcion que pone el usuario
+     
     if opcion == "1":
-        seguir_agregando = "si"
-        while seguir_agregando != "no":
-            agregar_producto()
-            seguir_agregando = input("Quieres agregar otro producto? si/no: ")
+        seguir_agregando = "si" #Variable para salir del bucle while de abajo
+        while seguir_agregando != "no": #Bucle para agregar productos segun las veces que el usuario quiera
+            Funciones.agregar_producto(inventario) #Aca se llama a la funcion que agrega los productos
+            seguir_agregando = input("Quieres agregar otro producto? si/no: ") #si pone no se sale del bucle
+
     elif opcion == "2":
         print("\nInventario")
-        mostrar_inventario()
+        Funciones.mostrar_inventario(inventario) #Aca se llama a la funcion que muestra el inventario
+
     elif opcion == "3":
         print("\nEstadisticas")
-        calcular_estadisticas()
+        Funciones.calcular_estadisticas(inventario) #Aca se llama a la funcion que calcula las estadisticas
+
     elif opcion == "4":
         print("Chao")
-        salir_bucle = 1
+        salir_bucle = 1 #Aca se cambia el valor de la variable para salir del programa
     else:
-        print("Ingresa una opcion valida")
+        print("Ingresa una opcion valida") #Esto se ejecuta si ingresa algo diferente a las opciones dadas
